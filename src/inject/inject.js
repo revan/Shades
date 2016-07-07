38de97293d0e2ff7188bafa5da5d9875572a5447
Revan Sopher <rsopher@gmail.com>
2016-07-07 14:58:04 -0400

    Skew.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -14,8 +14,6 @@ chrome.extension.sendMessage({}, function(response) {
 				$(img).wrap("<div id='portrait-canvas' style='position:absolute'>");
 				$('#portrait-canvas').wrap("<div style='height:" + img.height + "px;margin:20px'></div>");
 
-				console.log(event);
-
 				var shades = document.createElement('img');
 				shades.id = 'cool-shades';
 				shades.src = chrome.extension.getURL('shades.png');
@@ -24,17 +22,21 @@ chrome.extension.sendMessage({}, function(response) {
 					event.data[1].width + event.data[1].x) -
 					Math.min(event.data[0].x, event.data[1].x);
 
-				shades.height = Math.max(event.data[0].height + event.data[0].y,
+				shades.height = (2/3)*(Math.max(event.data[0].height + event.data[0].y,
 					event.data[1].height + event.data[1].y) -
-					Math.min(event.data[0].y, event.data[0].y);
+					Math.min(event.data[0].y, event.data[0].y));
 
 				shades.style.left = (img.offsetLeft + Math.min(event.data[0].x, event.data[1].x)) + 'px';
 				shades.style.top = 0;
 				shades.style.position = 'absolute';
 
+				var skew = Math.atan((event.data[0].y + event.data[0].height/2 - event.data[1].y - event.data[1].height/2) /
+					(event.data[0].x + event.data[0].width/2 - event.data[1].x - event.data[1].width/2));
+				shades.style.transform = 'skewY(' + skew + 'rad)';
+
 				document.querySelector('#portrait-canvas').appendChild(shades);
 
-				$('#cool-shades').animate({top: (img.offsetTop + Math.min(event.data[0].y, event.data[1].y)) + 'px'}, 3000);
+				$('#cool-shades').animate({top: 10 + (img.offsetTop + Math.min(event.data[0].y, event.data[1].y)) + 'px'}, 3000);
 			}
 		});
 
