d4a439ce104cbff9231f14df8c630b9d28e26a88
Revan Sopher <rsopher@gmail.com>
2016-07-07 04:08:18 -0400

    Eye tracking.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,6 +8,38 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
+		var img = document.querySelector(".infobox.vcard").querySelector("img");
+		img.id = 'wiki-portrait';
+		$(img).wrap("<div id='portrait-canvas' style='position:absolute'>");
+		// $('#portrait-wrapper').prepend("<div id='portrait-canvas' style='position:absolute;height:0;width:0;'></div>");
+
+		console.log(img);
+
+
+
+		var tracker = new tracking.ObjectTracker(['eye']);
+		tracker.setStepSize(1);
+		tracking.track('#wiki-portrait', tracker);
+		tracker.on('track', function(event) {
+			console.log(event);
+			event.data.forEach(function(rect) {
+				window.plot(rect.x, rect.y, rect.width, rect.height);
+			});
+		});
+		window.plot = function(x, y, w, h) {
+			var rect = document.createElement('div');
+			document.querySelector('#portrait-canvas').appendChild(rect);
+			rect.classList.add('rect');
+			rect.style.border = '2px solid #a64ceb';
+			// rect.top = '-1000px';
+			// rect.left = '-1000px';
+			rect.style.position = 'absolute';
+			rect.style.width = w + 'px';
+			rect.style.height = h + 'px';
+			rect.style.left = (img.offsetLeft + x) + 'px';
+			rect.style.top = (img.offsetTop + y) + 'px';
+		};
+
 	}
 	}, 10);
 });
