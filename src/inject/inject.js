08da206d0b6b580dfec7d3c9ff35c184d7b3799c
Revan Sopher <rsopher@gmail.com>
2016-07-07 05:06:16 -0400

    Slide in.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,31 +5,37 @@ chrome.extension.sendMessage({}, function(response) {
 
 		var img = document.querySelector(".infobox.vcard").querySelector("img");
 		img.id = 'wiki-portrait';
-		$(img).wrap("<div id='portrait-canvas' style='position:absolute'>");
-		$('#portrait-canvas').wrap("<div style='height:" + img.height + "px;margin:10px'></div>");
 
 		var tracker = new tracking.ObjectTracker(['eye']);
 		tracker.setStepSize(1);
 		tracking.track('#wiki-portrait', tracker);
 		tracker.on('track', function(event) {
-			console.log(event);
+			if (event.data.length >= 2) {
+				$(img).wrap("<div id='portrait-canvas' style='position:absolute'>");
+				$('#portrait-canvas').wrap("<div style='height:" + img.height + "px;margin:20px'></div>");
 
-			var shades = document.createElement('img');
-			shades.src = chrome.extension.getURL('shades.png');
+				console.log(event);
 
-			shades.width = Math.max(event.data[0].width + event.data[0].x,
-				event.data[1].width + event.data[1].x) -
-				Math.min(event.data[0].x, event.data[1].x);
+				var shades = document.createElement('img');
+				shades.id = 'cool-shades';
+				shades.src = chrome.extension.getURL('shades.png');
 
-			shades.height = Math.max(event.data[0].height + event.data[0].y,
-				event.data[1].height + event.data[1].y) -
-				Math.min(event.data[0].y, event.data[0].y);
+				shades.width = Math.max(event.data[0].width + event.data[0].x,
+					event.data[1].width + event.data[1].x) -
+					Math.min(event.data[0].x, event.data[1].x);
 
-			shades.style.left = (img.offsetLeft + Math.min(event.data[0].x, event.data[1].x)) + 'px';
-			shades.style.top = (img.offsetTop + Math.min(event.data[0].y, event.data[1].y)) + 'px';
-			shades.style.position = 'absolute';
+				shades.height = Math.max(event.data[0].height + event.data[0].y,
+					event.data[1].height + event.data[1].y) -
+					Math.min(event.data[0].y, event.data[0].y);
 
-			document.querySelector('#portrait-canvas').appendChild(shades);
+				shades.style.left = (img.offsetLeft + Math.min(event.data[0].x, event.data[1].x)) + 'px';
+				shades.style.top = 0;
+				shades.style.position = 'absolute';
+
+				document.querySelector('#portrait-canvas').appendChild(shades);
+
+				$('#cool-shades').animate({top: (img.offsetTop + Math.min(event.data[0].y, event.data[1].y)) + 'px'}, 3000);
+			}
 		});
 
 	}
